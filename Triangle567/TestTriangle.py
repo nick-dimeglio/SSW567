e86eb2587bbcd8cfd52259b502c7a197a74e2d0d
Nekolace <nickyd528@aol.com>
2022-09-21 02:39:02 -0400

    Updated triangle code and unit tests.

--- Triangle567/TestTriangle.py
+++ Triangle567/TestTriangle.py
@@ -14,19 +14,53 @@ from Triangle import classifyTriangle
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
+
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def testRightTriangles(self):
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right',
+                         '3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right',
+                         '5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(6, 10, 8), 'Right',
+                         '6,10,8 is a Right triangle')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def testIsoscelesTriangle(self):
+        self.assertEqual(classifyTriangle(3, 3, 4), 'Isoceles',
+                         '3,3,4 is an Isosceles triangle')
+        self.assertEqual(classifyTriangle(99, 99, 88), 'Isoceles',
+                         '3,3,4 is an Isosceles triangle')
+
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(3, 6, 4), 'Scalene',
+                         '3,6,4 is a Scalene triangle')
+        self.assertEqual(classifyTriangle(20, 29, 27), 'Scalene',
+                         '3,6,4 is a Scalene triangle')
+
+    def testEquilateralTriangles(self):
+        self.assertEqual(classifyTriangle(1, 1, 1),
+                         'Equilateral', '1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(200, 200, 200),
+                         'Equilateral', '200,200,200 should be equilateral')
+
+    def testNotTriangles(self):
+        self.assertEqual(classifyTriangle(1, 2, 10),
+                         'NotATriangle', '1,2,10 should not be a triangle')
+        self.assertEqual(classifyTriangle(0, 0, 0),
+                         'NotATriangle', '0,0,0 should not be a valid triangle.')
+
+    def testInvalidInputs(self):
+        self.assertEqual(classifyTriangle("k", 2, 10),
+                         'InvalidInput', 'k,2,10 should not be a valid input.')
+        self.assertEqual(classifyTriangle(1.0, 2, 10),
+                         'InvalidInput', '1.0,2,10 should not be a valid input.')
+        self.assertEqual(classifyTriangle(201, 200, 201),
+                         'InvalidInput', '201,200,201 should not be a valid input.')
+        self.assertEqual(classifyTriangle(-1, 0, 0),
+                         'InvalidInput', '-1,0,0 should not be a valid input.')
+
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
--- Triangle567/Triangle.py
+++ Triangle567/Triangle.py
@@ -9,49 +9,70 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+
+import heapq
+
+
+def classifyTriangle(a, b, c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
     you test cases. 
-    
+
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
+
       BEWARE: there may be a bug or two in this code
     """
 
+    # verify that all 3 inputs are integers
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not(isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+        return 'InvalidInput'
+
+    # ERROR: originally placed before check for valid integer inputs, potentially breakable code.
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+
+    # error found where b was set to check for b<=b rather than b<= 0
+
+    if a < 0 or b < 0 or c < 0:
         return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
+
+    # This information was not in the requirements spec but
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    # ERROR:found here where differences wer being checked rather than sums
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
+
+    # now we know that we have a valid triangle
+
+    # ADDED: code to order a,b, and c into smallest to largest respectively via heapification.
+    # This is done to ensure that the correct side is being checked for the right angle.
+    # It is also time efficient as heapification and sorting will only take O(n) rather than O(nlogn) time.
+    # However, it is a bit redundant because there is a strict 3 side triangle requirement.
+    # Still doing it out of good practice.
+    sides = [a, b, c]
+    heapq.heapify(sides)
+    a = heapq.heappop(sides)
+    b = heapq.heappop(sides)
+    c = heapq.heappop(sides)
+    # ERROR: found here, not checking other cases for equilateral
+    if a == b and c == a and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    # ERROR: found here, multiplying not using exponent.
+    elif ((a ** 2) + (b ** 2)) == (c ** 2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and (b != c) and (a != b):
         return 'Scalene'
     else:
         return 'Isoceles'
